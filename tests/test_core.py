f4273f971899a83f6c256dac96ad955c2ecb580b
Mitchell Stokes <mogurijin@gmail.com>
2014-05-23 00:50:36 -0700

    Adding a silly unit test just to get testing started.

---
+++ tests/test_core.py
@@ -0,0 +1,10 @@
+import unittest
+import yggdrasil
+
+
+class TestCore(unittest.TestCase):
+    def test_true(self):
+        self.assertEquals(1, 1)
+
+if __name__ == '__main__':
+    unittest.main()
